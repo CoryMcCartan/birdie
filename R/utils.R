a9370f9645a371d1e2a6d180058721e6328b2826
Cory McCartan <cory.mccartan@gmail.com>
2022-03-30 13:03:08 -0400

    work

--- R/utils.R
+++ R/utils.R
@@ -3,12 +3,12 @@
 check_vec = function(x) (is.character(x) | is.factor(x)) && !any(is.na(x))
 
 # Dev-facing helper for viewing
-print_cond = function(x, title=NULL) {
+print_cond = function(x, title=NULL, digits=1) {
     p_r = parent.frame()$p_r
     out = 100 * x %*% diag(1 / p_r)
     colnames(out) = names(p_r)
     if (!is.null(title)) cat(toupper(title), "\n")
-    print(round(out))
+    print(round(out, digits))
 }
 
 
