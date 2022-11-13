4b541c8d1ef937eaf2e348de52848271a5ff3d94
Cory McCartan <cory.mccartan@gmail.com>
2022-11-13 16:45:58 -0500

    better BISG matrix handling

--- R/baseline.R
+++ R/baseline.R
@@ -14,15 +14,20 @@
 #'   each value of `R` giving the conditional probabilities of R given Z and G.
 #'   Defaults to a ZIP code table from the 2010 decennial census.
 #' @param p_r a vector containing the marginal probabilities for each value of
-#'   `R`. Defaults to the demographics of the US.
+#'   `R`. Defaults to the demographics of the US if `est_r_gz` is `FALSE`.
+#' @param est_r_gz If `TRUE` estimate race prior `p_r` from distribution of `G` and `Z` in sample
 #' @param iterate how much to iteratively refine the race prior from the data. Set to 0 to disable.
+#' @param return_gzr whether to return an estimated p(G,Z|R) matrix as the
+#'   `p_gzr` attribute. Set to `FALSE` to save space if this matrix is not
+#'   needed.
 #'
 #' @return a tibble, with rows matching `data` and columns for the race probabilities
 #' @export
 predict_race_sgz = function(S, G, Z=NULL, data=NULL, p_rs=NULL, p_rgz=NULL,
                             p_r=c(white=0.630, black=0.121, hisp=0.173,
                                   asian=0.0478, aian=0.0072, other=0.0210),
-                            iterate=10L) {
+                            est_r_gz=TRUE, iterate=8L * (!est_r_gz && nrow(data) > 100),
+                            return_gzr=TRUE) {
     ## Parse and check input data frame ----------------
     if (missing(data)) cli_abort("{.arg data} must be provided.")
     S_vec = eval_tidy(enquo(S), data)
@@ -45,26 +50,28 @@ predict_race_sgz = function(S, G, Z=NULL, data=NULL, p_rs=NULL, p_rgz=NULL,
     ## Parse and check input probabilities ----------------
     if (missing(p_rs)) {
         S_vec = as.character(S_vec)
-        p_rs = census_surname_table(S_vec, as_name(enquo(S)), p_r)
+        p_rs = census_surname_table(S_vec, "last_name", p_r, flip=TRUE)
         S_vec[!S_vec %in% p_rs[[1]]] = "<generic>"
         S_vec = factor(S_vec, levels=p_rs[[1]])
+        p_sr = as.matrix(p_rs[, -1])
     } else {
         if (!is.data.frame(p_rs)) cli_abort("{.arg p_rs} must be a data frame.")
         if (!all(S_vec %in% p_rs[, 1]))
             cli_abort("Some names are missing from {.arg p_rs}.")
-    }
-    p_s = prop.table(table(S_vec))
-    p_sr = as.matrix(p_rs[, -1])
-    for (i in seq_along(p_r)) {
-        p_sr[, i] = p_sr[, i] * p_s
-        p_sr[, i] = p_sr[, i] / sum(p_sr[, i])
+
+        p_s = prop.table(table(S_vec))
+        p_sr = as.matrix(p_rs[, -1])
+        for (i in seq_along(p_r)) {
+            p_sr[, i] = p_sr[, i] * p_s
+            p_sr[, i] = p_sr[, i] / sum(p_sr[, i])
+        }
     }
 
     if (missing(p_rgz) && !missing(Z) && ncol(Z_df) == 2) { # TODO remove this (NC-specific)
         names(GZ)[1] = "zip"
         #p_rgz = census_zip_age_sex_table(GZ, GZ_vec, p_r, regularize)
     } else if (missing(p_rgz)) {
-        p_rgz = census_zip_table(G_vec, G_name, p_r)
+        p_rgz = census_zip_table(G_vec, G_name, p_r, counts=TRUE)
     } else {
         if (!is.data.frame(p_rs)) cli_abort("{.arg p_rgz} must be a data frame.")
         if (!all(colnames(GZ) %in% colnames(p_rgz)))
@@ -89,13 +96,17 @@ predict_race_sgz = function(S, G, Z=NULL, data=NULL, p_rs=NULL, p_rgz=NULL,
     match_idx[is.na(match_idx)] = match("<none>", p_rgz[[G_name]])
     p_rgz = p_rgz[match_idx, ]
 
-    p_gz = prop.table(table(GZ_vec))
-
     # flip which margin sums to 1
+    p_gz = prop.table(table(GZ_vec))
     p_gzr = as.matrix(select(p_rgz, .data$white:.data$other))
     for (i in seq_along(p_r)) {
         p_gzr[, i] = p_gzr[, i] * p_gz
         p_gzr[, i] = p_gzr[, i] / sum(p_gzr[, i])
+    }
+
+    if (isTRUE(est_r_gz)) {
+        p_r = colSums(p_rgz[, -1])
+        p_r = p_r / sum(p_r)
     }
 
     m_bisg = est_bisg(S_vec, GZ_vec, p_sr, p_gzr, p_r)
@@ -106,8 +117,12 @@ predict_race_sgz = function(S, G, Z=NULL, data=NULL, p_rs=NULL, p_rgz=NULL,
         }
     }
 
-    #list(pr=as_tibble(m_bisg), GZ=GZ_vec, S=p_sr[S_vec, ])
-    as_tibble(m_bisg)
+    out = as_tibble(m_bisg)
+    if (isTRUE(return_gzr)) {
+        rownames(p_gzr) = p_rgz[[G_name]]
+        attr(out, "p_gzr") = p_gzr
+    }
+    out
 }
 
 
