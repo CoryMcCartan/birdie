8877bd7c59bf383195ba50d5c0a4e021c85b56fb
Cory McCartan <cory.mccartan@gmail.com>
2022-11-15 18:43:30 -0500

    basic ME BISG test (#2)

--- tests/testthat/test-bisg.R
+++ tests/testthat/test-bisg.R
@@ -1,3 +1,10 @@
+log_score <- function(x, R) {
+    pr_act = as.matrix(x)[cbind(1:nrow(x), as.integer(R))]
+    pr_act[pr_act == 0] = 1e-6
+    mean(log(pr_act))
+}
+
+
 test_that("BISG results are directionally correct", {
     d = tibble(S=rep(c("Hernandez", "Mc Cartan"), 2),
                G=rep(c("78501", "50112"), each=2))
@@ -13,6 +20,21 @@ test_that("BISG results are directionally correct", {
     expect_gt(predict_race_sgz(S, G, data=tibble(S="LOCKLEAR", G="28715"))$pr_aian, 0.5)
     expect_gt(predict_race_sgz(S, G, data=tibble(S="WASHINGTON", G="98118"))$pr_black, 0.9)
     expect_gt(predict_race_sgz(S, G, data=tibble(S="CHEN", G="98118"))$pr_asian, 0.95)
+})
+
+test_that("Measurement error BISG model works", {
+    data("pseudo_vf")
+
+    p_r = prop.table(table(pseudo_vf$race))
+
+    pr_0 = predict_race_sgz(last_name, zip, data=pseudo_vf, p_r=p_r)
+    pr_me = predict_race_sgz_me(last_name, zip, data=pseudo_vf, p_r=p_r,
+                                warmup=100, iter=1000)
+
+    expect_true(all(diag(cor(pr_0, pr_me))[1:5] > 0.9))
+
+    # ME within 2% or better
+    expect_gt(log_score(pr_me, pseudo_vf$race), log_score(pr_0, pseudo_vf$race) - 0.02)
 })
 
 test_that("BISG results match `wru`", {
