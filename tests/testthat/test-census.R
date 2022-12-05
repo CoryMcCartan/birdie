a56ca9fbffe38060599b9c508aa7aa65e2a5c111
Cory McCartan <cory.mccartan@gmail.com>
2022-12-05 17:30:48 -0500

    cleanup

--- tests/testthat/test-census.R
+++ tests/testthat/test-census.R
@@ -1,3 +1,6 @@
+skip_on_cran()
+skip_if(Sys.getenv("CENSUS_API_KEY") == "")
+
 test_that("Census data can be downloaded", {
     expect_s3_class(census_race_geo_table("us"), "data.frame")
     expect_s3_class(census_race_geo_table("region"), "data.frame")
