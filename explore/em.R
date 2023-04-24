a431e3b3bae2487afc07b29db77f1b1ac70f62ea
Cory McCartan <cory.mccartan@gmail.com>
2023-04-24 19:22:05 -0400

    clarification

--- explore/em.R
+++ explore/em.R
@@ -26,6 +26,22 @@ x0 = birdie(r_probs, Y ~ 1, data)
 x1 = birdie(r_probs, Y ~ zip, data)
 x2 = birdie(r_probs, Y ~ white + black + hisp + (1|zip), data, ctrl=birdie.ctrl(abstol=1e-5))
 
+{
+    par(mfrow=c(3,2), mar=c(2, 2, 0.2, 0.2))
+    triangle = cbind(c(0, 0.5, 1), c(0, 1, 0))
+    for (r in 1:6) {
+        theta = t(coef(x2, subgroup=TRUE)[1:3, r, ])
+        plot(rbind(triangle, triangle[1, ]), type="l", col="#777777")
+        points(theta %*% triangle, cex=0.7, pch=16, col="#00000022")
+        points(coef(x2)[1:3, r] %*% triangle, cex=2, col="red", pch=8)
+        text(triangle, labels=toupper(rownames(coef(x1))[1:3]), cex=1.0)
+        text(0.1, 0.9, labels=names(p_r[r]), cex=2)
+    }
+    par(mfrow=c(1,1), mar=c(5, 4, 4, 2) + 0.1)
+}
+
+
+
 xr = list(
     true = with(d, prop.table(table(Y, race))),
     pool = coef(x0) %*% diag(colMeans(fitted(x0))),
